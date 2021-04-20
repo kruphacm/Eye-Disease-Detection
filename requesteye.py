8e9a7a347a4b67f6e098f93ae249466a09e21cb4
kruphacm <64674901+kruphacm@users.noreply.github.com>
2021-04-20 11:01:20 +0530

    Update requesteye.py

--- requesteye.py
+++ requesteye.py
@@ -1,6 +1,6 @@
 import requests
 
 url = 'http://localhost:5000/results'
-r = requests.post(url,json={'Enter the Symptom 1':0, 'Enter the Symptom 1':1, 'Enter the Symptom 1Enter the Symptom 1':2})
+r = requests.post(url,json={'Enter the Symptom 1':0, 'Enter the Symptom 2':1, 'Enter the Symptom 3':2})
 
 print(r.json())
